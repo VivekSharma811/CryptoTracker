1a62cdae593c315d4547c873ae35505cc50fc052
Vivek Sharma <vivek.sharma@mpokket.com>
2024-10-28 16:58:31 +0530

    LineChart added in CoinDetailScreen

--- app/src/main/java/com/hypheno/cryptotracker/crypto/presentation/coin_list/CoinListViewModel.kt
+++ app/src/main/java/com/hypheno/cryptotracker/crypto/presentation/coin_list/CoinListViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.viewModelScope
 import com.hypheno.cryptotracker.core.domain.util.onError
 import com.hypheno.cryptotracker.core.domain.util.onSuccess
 import com.hypheno.cryptotracker.crypto.domain.CoinDataSource
+import com.hypheno.cryptotracker.crypto.presentation.coin_detail.DataPoint
 import com.hypheno.cryptotracker.crypto.presentation.models.CoinUi
 import com.hypheno.cryptotracker.crypto.presentation.models.toCoinUi
 import kotlinx.coroutines.channels.Channel
@@ -16,6 +17,7 @@ import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 
 class CoinListViewModel(
     private val coinDataSource: CoinDataSource
@@ -51,7 +53,24 @@ class CoinListViewModel(
                     end = ZonedDateTime.now()
                 )
                 .onSuccess { history ->
-                    println(history)
+                    val dataPoints = history
+                        .sortedBy { it.dateTime }
+                        .map {
+                            DataPoint(
+                                x = it.dateTime.hour.toFloat(),
+                                y = it.priceUsd.toFloat(),
+                                xLabel = DateTimeFormatter
+                                    .ofPattern("ha\nM/d")
+                                    .format(it.dateTime)
+                            )
+                        }
+                    _state.update {
+                        it.copy(
+                            selectedCoin = it.selectedCoin?.copy(
+                                coinPriceHistory = dataPoints
+                            )
+                        )
+                    }
                 }
                 .onError { error ->
                     _events.send(CoinListEvent.Error(error))
